a3b37aee5649268cf229f28dccaa3aff9ba6f702
Type-32 <87076491+Type-32@users.noreply.github.com>
2024-05-30 23:53:22 +0800

    Init Commit

---
+++ Core/InstallationManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+
+namespace CRTLMCModInstaller.Core;
+
+public class InstallationManager
+{
+    public static void DownloadFileFromUrl(string url, string outputPath)
+    {
+        using (WebClient client = new WebClient())
+        {
+            try
+            {
+                Console.WriteLine("Downloading file from: " + url);
+                client.DownloadFileAsync(new Uri(url), outputPath);
+                Console.WriteLine("File downloaded successfully!");
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine("An error occurred while downloading the file:");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using CRTLMCModInstaller.Core;
+
+namespace CRTLMCModInstaller;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string id = "xYL9nIiR";
+        InstallationManager.DownloadFileFromUrl($"https://api.modrinth.com/v2/version/{id}", "Test");
+    }
+}
