f05e4d9ed038cf6adff8c53f13576d832584d689
Type-32 <87076491+Type-32@users.noreply.github.com>
2024-06-01 17:42:11 +0800

    Changed GitIgnore

--- Core/Managers/ModrinthManager.cs
+++ Core/Managers/ModrinthManager.cs
@@ -7,17 +7,32 @@ namespace CRTLMCModInstaller.Core;
 public class ModrinthManager
 {
     public static Uri BaseUrl = NetworkUtil.Morph(EnvUtil.TryGet(EnvReferences.StagingApiUrlBase));
+    public static string DownloadDirectory = Directory.GetCurrentDirectory();
 
-    public static bool TestRequest()
+    public static async Task<bool> TestRequest()
     {
         using (HttpClient client = new HttpClient())
         {
             client.BaseAddress = BaseUrl;
 
-            HttpResponseMessage msg = new HttpResponseMessage();
-            // msg.Headers.Add();
+            HttpResponseMessage res = await client.GetAsync(new Uri(""));
+            return res.IsSuccessStatusCode;
         }
+    }
 
-        return true;
+    public static async Task<HttpContent> SearchProjects(string query)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            client.BaseAddress = BaseUrl;
+            Uri queryUri = new NetworkUtil.QueryBuilder(new Uri("search"))
+                .AddQuery("query", query)
+                .Get();
+            HttpRequestMessage req = NetworkUtil.BuildRequest(NetworkUtil.RequestMethod.Get, queryUri);
+            
+            HttpResponseMessage res = await client.SendAsync(req);
+
+            return res.Content;
+        }
     }
 }
--- Core/Utility/NetworkUtil.cs
+++ Core/Utility/NetworkUtil.cs
@@ -1,11 +1,57 @@
+using System.Web;
+
 namespace CRTLMCModInstaller.Core.Utility;
 
 public class NetworkUtil
 {
+    public enum RequestMethod
+    {
+        Get,
+        Post
+    }
     
     public static Uri Morph(string uri)
     {
         string head = uri.Contains("https://") || uri.Contains("http://") ? "" : "https://";
         return new Uri(head + uri);
+    }
+
+    public static HttpRequestMessage BuildRequest(RequestMethod method, Uri requestUri, bool withUserAgent = true)
+    {
+        HttpRequestMessage msg =
+            new HttpRequestMessage(new HttpMethod(method == RequestMethod.Get ? "GET" : "POST"), requestUri);
+        
+        if (withUserAgent)
+            msg.Headers.Add("User-Agent", EnvUtil.TryGet("USER_AGENT_HEADER_VALUE", "Type-32/CRTLMCModInstaller/0.1.0 (type32alt@outlook.com)"));
+
+        return msg;
+    }
+
+    public class QueryBuilder
+    {
+        protected readonly Uri baseUri;
+
+        public QueryBuilder(Uri uri)
+        {
+            this.baseUri = uri;
+        }
+        
+        public QueryBuilder AddQuery(string name, string value)
+        {
+            var httpValueCollection = HttpUtility.ParseQueryString(baseUri.Query);
+
+            httpValueCollection.Remove(name);
+            httpValueCollection.Add(name, value);
+
+            var ub = new UriBuilder(baseUri);
+            ub.Query = httpValueCollection.ToString();
+
+            return new QueryBuilder(ub.Uri);
+        }
+
+        public Uri Get()
+        {
+            return baseUri;
+        }
     }
 }
